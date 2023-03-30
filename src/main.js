0049aa5c07fd0357c12030c3efe7032fbf97db06
Davyd Kohut <davyd.kohut@gmail.com>
2023-03-31 01:09:30 +0300

    0.0.2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-//
+/**
+ * @param {string} name
+ * @param {string} repoUrl
+ * @param {string} rootPath
+ */
+export async function pakAddFromRepoUrl(name, repoUrl, rootPath) {
+    const { default: clone } = await import("git-clone/promise.js");
+    const { default: fs } = await import("fs");
+
+    const path = `${rootPath}/.паки/${name}`;
+
+    fs.rmSync(path, { recursive: true, force: true });
+
+    await clone(repo, path);
+}
+
+/**
+ * @param {string} inputModulePath
+ * @param {string} outputModulePath
+ */
+export async function pakCloud(inputModulePath, outputModulePath) {
+    //
+}
